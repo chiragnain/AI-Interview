b2dca28b09daf3cb51cc63d37517d0e46338ece9
Chirag Nain <chiraghalnm@gmail.com>
2024-07-15 17:40:34 +0530

    final

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
-import {Button} from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h2>dhho</h2>
-      <Button>Subsciribe</Button>
+    <div style={{ backgroundImage: "url('/front.png')", height: '90vh', backgroundSize: 'cover' }} className="flex justify-center">
+      <Link href="/dashboard">
+        <Button style={{ padding: '40px 80px', fontSize: '20px'}}>Open Dashboard</Button>
+      </Link>
     </div>
   );
 }
